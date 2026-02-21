36b6430f0af2015676b2a84afa533928c0fea34d
Kai <dk786161616@gmail.com>
2026-02-22 01:21:48 +0530

    setup spring grpc

---
+++ grpcapp/src/main/java/org/example/grpcapp/GrpcappApplication.java
@@ -0,0 +1,13 @@
+package org.example.grpcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcappApplication.class, args);
+    }
+
+}
